c7a951d51e859656db45654ee3170648883d99ae
Sujinda Jaipinta <mumuch4321@gmail.com>
2025-07-24 14:10:55 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Resume Sujinda',
+  description: 'Created by Sujinda',
+  generator: 'Resume Sujinda',
 }
 
 export default function RootLayout({
